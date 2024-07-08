8459093b98c93397e2fd9d412c1e75dda41ead2f
Leonhard Astrea <xtukesx@gmail.com>
2024-07-08 20:26:27 +0300

    [iter2] Statictest fixes: renamed variables

--- internal/app/handlers/handlers_test.go
+++ internal/app/handlers/handlers_test.go
@@ -13,8 +13,8 @@ import (
 )
 
 func TestShortenURL(t *testing.T) {
-	validHttpsHost := "https://somehost/"
-	expectedBodyRxString := fmt.Sprintf("^(http|https)%s[a-z0-9]{%d}$", strings.Replace(validHttpsHost, "https", "", 1), linkLength)
+	validHTTPSHost := "https://somehost/"
+	expectedBodyRxString := fmt.Sprintf("^(http|https)%s[a-z0-9]{%d}$", strings.Replace(validHTTPSHost, "https", "", 1), linkLength)
 	expectedBodyRx := regexp.MustCompile(expectedBodyRxString)
 	tests := []struct {
 		name                 string
@@ -24,11 +24,11 @@ func TestShortenURL(t *testing.T) {
 		expectedCode         int
 		expectedErrorMessage string
 	}{
-		{name: "Valid http request", requestHost: strings.Replace(validHttpsHost, "https", "http", 1), requestMethod: http.MethodPost, requestBody: "http://example.com", expectedCode: http.StatusCreated},
-		{name: "Valid https request", requestHost: validHttpsHost, requestMethod: http.MethodPost, requestBody: "https://example.com", expectedCode: http.StatusCreated},
-		{name: "Bad request, body contains not a URL", requestHost: validHttpsHost, requestMethod: http.MethodPost, requestBody: "not a URL", expectedCode: http.StatusBadRequest, expectedErrorMessage: "Invalid URL"},
-		{name: "Bad request, not supported method", requestHost: validHttpsHost, requestMethod: http.MethodGet, requestBody: "http://example.com", expectedCode: http.StatusBadRequest, expectedErrorMessage: "Not supported"},
-		{name: "Not found, wrong method", requestHost: validHttpsHost + "/some/deeper/path", requestMethod: http.MethodPost, requestBody: "http://example.com", expectedCode: http.StatusNotFound},
+		{name: "Valid http request", requestHost: strings.Replace(validHTTPSHost, "https", "http", 1), requestMethod: http.MethodPost, requestBody: "http://example.com", expectedCode: http.StatusCreated},
+		{name: "Valid https request", requestHost: validHTTPSHost, requestMethod: http.MethodPost, requestBody: "https://example.com", expectedCode: http.StatusCreated},
+		{name: "Bad request, body contains not a URL", requestHost: validHTTPSHost, requestMethod: http.MethodPost, requestBody: "not a URL", expectedCode: http.StatusBadRequest, expectedErrorMessage: "Invalid URL"},
+		{name: "Bad request, not supported method", requestHost: validHTTPSHost, requestMethod: http.MethodGet, requestBody: "http://example.com", expectedCode: http.StatusBadRequest, expectedErrorMessage: "Not supported"},
+		{name: "Not found, wrong method", requestHost: validHTTPSHost + "/some/deeper/path", requestMethod: http.MethodPost, requestBody: "http://example.com", expectedCode: http.StatusNotFound},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -56,9 +56,9 @@ func TestShortenURL(t *testing.T) {
 
 func TestGetOriginalURL(t *testing.T) {
 	validHost := "https://somehost/"
-	validOriginalUrl := "https://example.com"
+	validOriginalURL := "https://example.com"
 
-	request := httptest.NewRequest(http.MethodPost, validHost, strings.NewReader(validOriginalUrl))
+	request := httptest.NewRequest(http.MethodPost, validHost, strings.NewReader(validOriginalURL))
 	response := httptest.NewRecorder()
 	defer response.Result().Body.Close()
 
@@ -74,16 +74,16 @@ func TestGetOriginalURL(t *testing.T) {
 		expectedCode         int
 		expectedErrorMessage string
 	}{
-		// {name: "Valid request", requestHost: shortenedURL, requestMethod: http.MethodGet, expectedCode: http.StatusTemporaryRedirect},
-		// {name: "Bad request, not supported method", requestHost: shortenedURL, requestMethod: http.MethodPost, expectedCode: http.StatusBadRequest, expectedErrorMessage: "Not supported"},
+		{name: "Valid request", requestHost: shortenedURL, requestMethod: http.MethodGet, expectedCode: http.StatusTemporaryRedirect},
+		{name: "Bad request, not supported method", requestHost: shortenedURL, requestMethod: http.MethodPost, expectedCode: http.StatusBadRequest, expectedErrorMessage: "Not supported"},
 		{name: "Not found", requestHost: validHost + "invalidId", requestMethod: http.MethodGet, expectedCode: http.StatusBadRequest, expectedErrorMessage: "Link not found"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			request := httptest.NewRequest(tt.requestMethod, tt.requestHost, nil)
 			shortenSplit := strings.Split(tt.requestHost, "/")
-			requestedId := shortenSplit[len(shortenSplit)-1]
-			request.SetPathValue("id", requestedId)
+			requestedID := shortenSplit[len(shortenSplit)-1]
+			request.SetPathValue("id", requestedID)
 			response := httptest.NewRecorder()
 			defer response.Result().Body.Close()
 
@@ -93,7 +93,7 @@ func TestGetOriginalURL(t *testing.T) {
 			if tt.expectedCode == http.StatusTemporaryRedirect {
 				response.Header()
 				actualLocation := response.Header().Get("Location")
-				assert.Equal(t, validOriginalUrl, actualLocation, "Expected Location header to contain [%v], got [%v]", validOriginalUrl, actualLocation)
+				assert.Equal(t, validOriginalURL, actualLocation, "Expected Location header to contain [%v], got [%v]", validOriginalURL, actualLocation)
 			}
 
 			if tt.expectedErrorMessage != "" {

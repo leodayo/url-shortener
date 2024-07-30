0859c894be4c537bff692692b8365f96e2544508
Leonhard Astrea <xtukesx@gmail.com>
2024-07-30 03:30:31 +0300

    [iter6] Added logger. Added middleware to log request and response info

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,12 +5,17 @@ import (
 
 	"github.com/leodayo/url-shortener/internal/app/config"
 	"github.com/leodayo/url-shortener/internal/app/handlers"
+	"github.com/leodayo/url-shortener/internal/logger"
 )
 
 func Run() error {
 	config.ParseFlags()
 	err := config.ParseEnv()
 	if err != nil {
+		return err
+	}
+
+	if err := logger.Initialize("debug"); err != nil {
 		return err
 	}
 
--- internal/app/handlers/router.go
+++ internal/app/handlers/router.go
@@ -5,10 +5,13 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/leodayo/url-shortener/internal/app/config"
+	"github.com/leodayo/url-shortener/internal/logger"
 )
 
 func MainRouter() http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(logger.ResponseLogger, logger.RequestLogger)
 
 	r.Post("/", ShortenURL)
 	r.Get(config.ExpandPath.Path+"/{id}", GetOriginalURL)
